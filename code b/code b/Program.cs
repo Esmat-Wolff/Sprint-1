e78530aee9aff99f7de50f2c8d9d4fc9a1347889
Esmat-Wolff <64648319+Esmat-Wolff@users.noreply.github.com>
2020-05-08 12:26:07 +0200

    Sprint 1 und Aufgabe 6

---
+++ code b/code b/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 7, 1, 15 ,0,12,23,25,17,16};
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if(arr[i] <arr[j])
+                    {
+                        temp = arr[i];
+
+                        arr[i] = arr[j];
+
+                        arr[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("{0},", arr[i]);
+                Console.ReadKey();
+            }
+        }
+    }
+}
